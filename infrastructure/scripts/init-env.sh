0edbb9bd5499e022d0477bc7498678ae2eb88968
Chris van Wyk <chris@jib.co.za>
2020-09-04 16:39:24 +0200

    Fix resourceGroupName

--- infrastructure/scripts/init-env.sh
+++ infrastructure/scripts/init-env.sh
@@ -179,7 +179,7 @@ determineResourceGroup() {
             "It appears you aren't currently running in a Microsoft Learn sandbox." \
             "Any Azure resources provisioned by this script will result in charges" \
             "to your Azure subscription.${defaultTextStyle}"
-        resourceGroupName=$moduleName"-rg"
+        resourceGroupName="$moduleName-rg"
     else
         resourceGroupName=$existingResourceGroup
     fi
